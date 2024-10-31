5f31cb86cfcf951d13fba3a38da9f32bc1e9d2fd
Home <home@MacBook-Pro-home.local>
2024-10-31 18:42:52 +0300

    fixed an issue with immunability during deleteHero function

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,16 +1,38 @@
 import {useHttp} from '../../hooks/http.hook';
 import { useEffect, useCallback} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { createSelector } from 'reselect';
 import { heroesFetching, heroesFetched, heroesFetchingError } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
 const HeroesList = () => {
-    const {heroes, heroesLoadingStatus, filters, activeFilter} = useSelector(state => state);
+    const {heroes, heroesLoadingStatus, filters} = useSelector(state => state);
     const dispatch = useDispatch();
     const {request} = useHttp();
     let filter = [];
+    const filteredHeroesSelector = createSelector(
+        (state) => state.heroes,
+        (state) => state.activeFilter,
+        (arr, filter) => {
+            if (arr.length === 0) {
+                return <h5 className="text-center mt-5">Героев пока нет</h5>
+            }
+            console.log('render');
+            
+            return arr.map(({id, element, ...props}) => {
+                if (element == filter || element == 'all' || filter == 'all') {
+                    return <HeroesListItem 
+                                key={id}
+                                element={element}
+                                {...props}
+                                onDelete={() => deleteHero(id)}/>
+                }
+            })
+        }
+    )
+
+    const elements = useSelector(filteredHeroesSelector);
 
     useEffect(() => {
         dispatch(heroesFetching());
@@ -26,17 +48,24 @@ const HeroesList = () => {
         // eslint-disable-next-line
     }, []);
 
-    const deleteHero = useCallback(id => {
+    let newData = {
+        newHeroes:[],
+        newFilters:[]
+    }
+
+    const deleteHero = id => {
         request(`http://localhost:3001/heroes/${id}`, "DELETE")
-            .then(() => heroes.map((hero, i) => {
-                if (hero.id == id) {
-                    heroes.splice(i, 1);
-                    filters.splice(filters.indexOf(hero.element), 1)
+            .then(() => {heroes.map((hero, i) => {
+                if (hero.id !== id) {
+                    newData.newHeroes.push(hero);
+                    newData.newFilters.push(filters[i])
                 }
-                dispatch(heroesFetched(heroes, filters))
-            }))
+                return newData;
+            })
+            dispatch(heroesFetched(newData.newHeroes, newData.newFilters))
+        })
             .catch(() => dispatch(heroesFetchingError()))
-    })
+    }
 
     if (heroesLoadingStatus === "loading") {
         return <Spinner/>;
@@ -44,23 +73,6 @@ const HeroesList = () => {
         return <h5 className="text-center mt-5">Ошибка загрузки</h5>
     }
 
-    const renderHeroesList = (arr, filter) => {
-        if (arr.length === 0) {
-            return <h5 className="text-center mt-5">Героев пока нет</h5>
-        }
-        
-        return arr.map(({id, element, ...props}) => {
-            if (element == filter || element == 'all' || filter == 'all') {
-                return <HeroesListItem 
-                            key={id}
-                            element={element}
-                            {...props}
-                            onDelete={() => deleteHero(id)}/>
-            }
-        })
-    }
-
-    const elements = renderHeroesList(heroes, activeFilter);
     return (
         <ul>
             {elements}
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux';
 import reducer from '../reducers';
+import { configureStore } from '@reduxjs/toolkit';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = configureStore({
+    reducer: reducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware(),
+    devTools: process.env.NODE_ENV !== 'production'
+})
 
 export default store;
